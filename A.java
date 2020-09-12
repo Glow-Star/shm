290e7eff65925f4d64beeb67a8c90c20162e026d
YHM001223hm <66984733+YHM001223hm@users.noreply.github.com>
2020-09-12 14:34:33 +0800

    Add files via upload

---
+++ A.java
@@ -0,0 +1,15 @@
+public class A{
+	public static void main(String[] args) {
+	/*int num1 = 2;
+	int num2 = 3;
+	int result = num1+num2;
+	System.out.println(result);*/
+         	int x=10;
+         	int y=20;
+	System.out.println(x>y);
+	System.out.println(x<y);
+	System.out.println(x==y);
+	System.out.println(x!=y);
+
+}
+}
